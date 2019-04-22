ed334d675288ab3dc75be9c2823f8eb380e841bd
Felipe Cisotto <felipecisotto@MacBook-Pro-de-Felipe.local>
2019-04-22 18:42:19 -0300

    first commit

---
+++ src/main/java/dev/felipe/eletronicpoint/SpringAdvancedApiStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.felipe.eletronicpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedApiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdvancedApiStudyApplication.class, args);
+	}
+
+}
